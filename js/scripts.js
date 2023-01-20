dc9648c088a60eb0a5ab06b450bc00bfa01f660a
Nicolas <nicolasmdeniel@gmail.com>
2023-01-20 20:42:08 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -35,15 +35,18 @@ function redirect() {
 // }
 
 let btns = document.querySelectorAll("#btn"); 
+let p = document.querySelectorAll("#p");
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.display = "none";
+}
+
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function () {
-        let p = document.querySelectorAll("#p");
-        if (!p[i].className) {
-            p[i].classList.toggle("active");
-            p[i].style.display = "none";
-        } else {
-            p[i].classList.remove("active");
+        if (p[i].style.display = "none") {
             p[i].style.display = "block";
+        } else {
+            p[i].style.display = "none";
         }
         })
     }
